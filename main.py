bf379c4bd7e4222d6ecc2ab52c676afa553a73f5
jaysonmoon <PA$$word112306>
2020-12-04 15:11:22 -0500

    init

--- main.py
+++ main.py
@@ -1,45 +1,31 @@
-# arr = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1]
-# frame = int(input("Enter number of frames: "))
-# opr = []
-# remainder = [None for i in range(frame)]
-# page_faults = 0
-# print(f"Start: Memory is: {opr}")
-#
-# # for i in range(len(arr)):
-# #     if arr[i] not in opr:
-# #         if len(opr) < frame:
-# #             page_faults += 1
-# #             opr.append(arr[i])
-# #             print(f'{arr[i]}: Memory is: {opr}: # of Page Fault(s): {page_faults}')
-# #         else:
-# #             for j in range(len(arr)):
-# #                 if opr[j] not in arr[i + 1:]:  # if target element is not in the rest of array, replace it
-# #                     opr[j] = arr[i]
-# #                     break
-# #                 else:
-# #                     page_faults += 1
-# #                     opr[j] = arr[i + 1:].index([remainder[j]])
-# # print(opr)
+frame = int(input("Number of frames: "))
 
-f = []
-fault = 0
-pf = 'No'
-storage = int(input("Enter frames: "))
+count = 0
+memory = []
+faults = 0
+x = 0
 
-for i in range(storage):
-    occurrence = [None for i in range(storage)]
+arr = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1]
 
-for i in range(storage):
-    if storage[i] not in f:
-        if len(f) < storage:
-            f.append(storage[i])
-        else:
-            for x in range(len(f)):
-                if f[x] not in storage[i+1]:
-                    f[x] = storage[i]
-                    break
-                else:
-                    occurrence[x] = storage[i+1:].index(f[x])
+for page in arr:
+    if memory.count(page) == 0 and count < frame:
+        memory.append(page)
+        count += 1
+        faults += 1
+        print(memory)
+    elif memory.count(page) == 0 and count == frame:
+        future = -1
+        for i in memory:
+            if arr[x:].count(i) == 0:
+                evictedPage = i
+                break
             else:
-                f[occurrence.index(max(occurrence))] = storage[i]
-print('hello world')
+                index = arr[x:].index(i)
+            if index > future:
+                future = index
+                evictedPage = i
+    elif memory.count(page) > 0:
+        pass
+    x += 1
+
+print(faults)
