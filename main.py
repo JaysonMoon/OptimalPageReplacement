ec94eae03147c9d7bf52561b96e0702f9939ddd0
JaysonM <63750148+JaysonMoon@users.noreply.github.com>
2020-12-05 21:42:27 -0500

    Optimal Page Replacement

--- main.py
+++ main.py
@@ -1,31 +1,61 @@
-frame = int(input("Number of frames: "))
+def optimal_page_replacement():
+    arr = []
+    a = input("Enter a reference string: ")
+    for i in range(len(a)):
+        arr.append(int(a[i]))  # convert user entry into int list
+    frame = int(input("Enter the # of frames: "))
+    page_faults = 0
+    asterisk = '*'
+    opr = [asterisk] * frame  # new list with * and length specified by user
 
-count = 0
-memory = []
-faults = 0
-x = 0
+    print("\nRunning simulation: ")
+    print(f"\nStart: Memory is: {' '.join(map(str, opr))}")
 
-arr = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1]
-
-for page in arr:
-    if memory.count(page) == 0 and count < frame:
-        memory.append(page)
-        count += 1
-        faults += 1
-        print(memory)
-    elif memory.count(page) == 0 and count == frame:
-        future = -1
-        for i in memory:
-            if arr[x:].count(i) == 0:
-                evictedPage = i
+    for number_in_array in range(len(arr)):
+        questionable_element = 0
+        for index_in_frame in range(frame):
+            if opr[index_in_frame] == arr[number_in_array]:
+                questionable_element = 1
                 break
-            else:
-                index = arr[x:].index(i)
-            if index > future:
-                future = index
-                evictedPage = i
-    elif memory.count(page) > 0:
-        pass
-    x += 1
 
-print(faults)
+        if questionable_element == 0:
+            is_it_fault = False
+            position = asterisk
+            for element in range(frame):
+                if opr[element] == asterisk:
+                    is_it_fault = True
+                    position = element
+
+            if not is_it_fault:
+                furthest_out = 0
+                most_furthest_out = asterisk
+                for element in range(frame):
+                    if opr[element] != asterisk:
+                        found = False  # element we're trying to put in is not in the page
+                        for m in range(number_in_array,
+                                       len(arr)):  # look for number from position until the end of array
+                            if arr[m] == opr[element]:
+                                found = True
+                                if m > furthest_out:
+                                    furthest_out = m
+                                    most_furthest_out = element
+                                break
+                        if not found:
+                            most_furthest_out = element
+                            break
+
+                position = most_furthest_out
+
+            page_faults += 1
+            opr[position] = arr[number_in_array]
+            print(f"{arr[number_in_array]}: Memory is: {' '.join(map(str, opr))[::-1]}: "
+                  f"Page Fault: (Number of Page Faults: {page_faults})")
+
+        else:
+            print(f"{arr[number_in_array]}: Memory is: {' '.join(map(str, opr))[::-1]}: "
+                  f"Hit: (Number of Page Faults: {page_faults})")
+
+    print(f"Total Number of Page Faults: {page_faults}")
+
+
+optimal_page_replacement()
